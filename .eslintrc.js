8a0953f4bd4986cf46c07d1c43b078b388204492
as-you-say <dgf5820@naver.com>
2021-06-01 06:29:13 +0900

    Update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,14 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
-  'prettier/prettier': [
-    'error',
-    {
-      endOfLine: 'auto',
-    },
-  ],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    'no-unused-vars': 0,
+    '@typescript-eslint/no-unused-vars': ['off'],
+  },
 };
